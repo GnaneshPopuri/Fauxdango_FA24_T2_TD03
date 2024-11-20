38ce3cd1c2769af14af0ccfebbb1404a4ac389e4
pnc1921 <pnc1921@gmail.com>
2024-11-20 15:21:25 -0500

    TD00- Team number and members listed

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("===========================");
-        System.out.println("Team X"); // TODO put your team number
+        System.out.println("Team 2"); // TODO put your team number
 
         // TODO: List your team here
-        System.out.println("Jo Student (jxs5123@psu.edu)");
-        System.out.println("===========================");
-        System.out.println();
+        System.out.println("Prince Christian (pnc15@psu.edu)");
+        System.out.println("Diane Mathew  (dmm7029@psu.edu)");
+        System.out.println("Gnanesh Popuri (gjp5376@psu.edu)");
+        System.out.println("Joshua Waldron (jsw5796.edu)");
+
 
         Fauxdango f = new Fauxdango();
         f.demo();
