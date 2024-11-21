88a07b0640246e2aed892e6b03dd9091fa40f9a8
joshua Waldron <jshawnwaldron@gmail.com>
2024-11-20 22:32:00 -0500

    fixed errors

--- src/Controller/Fauxdango.java
+++ src/Controller/Fauxdango.java
@@ -16,6 +16,10 @@ public class Fauxdango {
     MenuChoice choiceMainListAllTheaters = menuMain.addMenuChoice("List All Theaters");
     MenuChoice choiceMainListAllActors = menuMain.addMenuChoice("List All Actors");
     MenuChoice choiceMainListAllShowings = menuMain.addMenuChoice("List All Showings");
+    MenuChoice choiceSearchActorsByName = menuMain.addMenuChoice("Search Actors By Name");
+    MenuChoice choiceSearchTheatersByName = menuMain.addMenuChoice("Search Theaters By Name");
+    MenuChoice choiceSearchTheatersByZipcode = menuMain.addMenuChoice("Search Theaters By Zipcode");
+    MenuChoice choiceSearchMoviesByTitle = menuMain.addMenuChoice("Search Movies By Title");
     MenuChoice choiceMainExit = menuMain.getMenuChoiceQuit();
 
     public void demo() {
@@ -58,10 +62,53 @@ public class Fauxdango {
                 for (Showing showing : Datastore.getShowings()) {
                     System.out.println(showing.toString());
                 }
+            } else if (chosen == choiceSearchActorsByName) {
+                System.out.print("Enter part of the name: : ");
+                String actorName = scanner.nextLine().toLowerCase();
+                boolean found = false;
+                for (Actor actor : Datastore.searchActorsByName(actorName)) {
+                    System.out.println(actor.toString());
+                    found = true;
+                }
+                if (!found) {
+                    System.out.println("No actors found matching: " + actorName);
+                }
+            } else if (chosen == choiceSearchTheatersByName) {
+                System.out.print("Enter part of the name: ");
+                String theaterName = scanner.nextLine().toLowerCase();
+                boolean found = false;
+                for (Theater theater : Datastore.searchTheatersByName(theaterName)) {
+                    System.out.println(theater.toString());
+                    found = true;
+                }
+                if (!found) {
+                    System.out.println("No theaters found matching: " + theaterName);
+                }
+            } else if (chosen == choiceSearchTheatersByZipcode) {
+                System.out.print("Enter zipcode: ");
+                String zipcode = scanner.nextLine();
+                boolean found = false;
+                for (Theater theater : Datastore.searchTheatersByZipcode(zipcode)) {
+                    System.out.println(theater.toString());
+                    found = true;
+                }
+                if (!found) {
+                    System.out.println("No theaters found in zipcode: " + zipcode);
+                }
+            } else if (chosen == choiceSearchMoviesByTitle) {
+                System.out.print("Enter part of the movie title: ");
+                String movieTitle = scanner.nextLine().toLowerCase();
+                boolean found = false;
+                for (Movie movie : Datastore.searchMoviesByTitle(movieTitle)) {
+                    System.out.println(movie.toString());
+                    found = true;
+                }
+                if (!found) {
+                    System.out.println("No movies found matching: " + movieTitle);
+                }
             } else if (chosen == choiceMainExit) {
                 System.out.println("Goodbye");
             }
         }
     }
 }
-
